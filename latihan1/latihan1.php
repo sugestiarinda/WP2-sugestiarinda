2952c0e48d48f96e6e47a89548d7ce19289bdb3d
sugestiarinda <90609856+sugestiarinda@users.noreply.github.com>
2021-10-06 14:38:06 +0700

    Add files via upload

---
+++ latihan1/latihan1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1> Perkenalkan<h1>";
+        echo "Nama saya Sugesti Arinda Kusumawardani
+            Kelas 19.3A.37
+            Nim 19200673";
+    }
+}
